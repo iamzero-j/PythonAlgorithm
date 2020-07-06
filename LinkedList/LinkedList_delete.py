d27a1ab97ccd486c809df50ec5815552c65033c3
iamzero-j <wjd20048@naver.com>
2020-07-06 22:57:03 +0900

    Create LinkedList_delete.py
    프로그래머스 알고리즘 - LinkedList

---
+++ LinkedList/LinkedList_delete.py
@@ -0,0 +1,17 @@
+def popAt(self, pos): # 삭제 노드
+    if pos < 1 or pos > self.nodeCount: #영역 밖
+        raise IndexError
+    else:
+        if pos == 1:
+            curr = self.head
+            self.head = curr.next
+            if self.nodeCount == 1:
+                self.tail = None
+        else:
+            prev = self.getAt(pos - 1)
+            curr = prev.next
+            prev.next = curr.next
+            if pos == self.nodeCount:
+                self.tail = prev
+        self.nodeCount -= 1
+    return curr.data
