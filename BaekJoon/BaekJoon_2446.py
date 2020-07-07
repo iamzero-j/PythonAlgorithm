b6d5e774197414e180f2a0af0dda29b2b44a90d6
iamzero-j <wjd20048@naver.com>
2020-07-07 17:35:08 +0900

    Create BaekJoon_2446.py

---
+++ BaekJoon/BaekJoon_2446.py
@@ -0,0 +1,12 @@
+# 출처 : 백준 -> https://www.acmicpc.net/problem/2446
+
+#별찍기 - 모래시계 모양 
+N=int(input())
+
+for i in range(0,N): 
+    print(" " * i, end="")
+    print("*" * ((2*N-1)-(2*i)))
+    
+for j in range(N-2,-1,-1):
+    print(" " * j, end="")
+    print("*" * ((2*N-1)-(2*j)))
