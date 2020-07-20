f3045d8587829cb0271c28fb6e95739fe8a27743
iamzero-j <wjd20048@naver.com>
2020-07-20 17:34:15 +0900

    Create BaekJoon_1152.py
    백준 1152번 - 단어의 개수 Python

---
+++ BaekJoon/BaekJoon_1152.py
@@ -0,0 +1,10 @@
+# 출처 : 백준 -> 문자열 : 1152번 https://www.acmicpc.net/problem/1152
+
+#단어의 개수
+#공백이 연속적으로 올 수 없음
+
+#ins=input().split(" ") 사용시 아무것도 안 치고 출력 하였을 때 0 이 아닌 1이 나옴
+
+ins=input().split()
+
+print(len(ins))
